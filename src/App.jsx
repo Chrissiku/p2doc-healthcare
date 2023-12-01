352f08899ff35e16cd75c081b4a1a38833c292c5
Sunday P. Afolabi <spog4true@gmail.com>
2023-12-01 05:11:23 -0800

    Start services section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Services from "./services/Services";
+
 const App = () => {
   return (
     <>
+      <Services />
       <h1 className="text-3xl font-bold underline text-green-400">Hello world!</h1>
     </>
   );
