e697ebdededc916fa807ec583d46dccbdb82771f
ginabeki <ginabeki3@gmail.com>
2023-12-01 15:41:39 +0300

    COnfigure color and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+       poppins: ['Poppins', 'sans-serif'],
+    },
+    colors: {
+      'og-blue': '#41cbe2',
+      'light-dark': '#585858',
+      'light-pink': '#F39F9F'
+    },
     extend: {},
   },
   plugins: [],
