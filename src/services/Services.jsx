d836a479ae9ca0b011649183c5e6161a88cff01c
Sunday P. Afolabi <spog4true@gmail.com>
2023-12-02 01:56:47 -0800

    PR issues resolved

--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -5,25 +5,25 @@ const Services = () => {
 
     const serviceElements = servicesConstnat?.map(service => {
         return (
-            <div class="relative pl-16" key={service.title}>
-                <dt class="text-base font-semibold leading-7 text-gray-900">
-                    <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+            <div className="relative pl-16" key={service.title}>
+                <dt className="text-base font-semibold leading-7 text-gray-900">
+                    <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                         <img src={service.image} alt={service.title} />
                     </div>
                     {service.title}
                 </dt>
-                <dd class="mt-2 text-base leading-7 text-gray-600">{service.paragraph}</dd>
+                <dd className="mt-2 text-base leading-7 text-gray-600">{service.paragraph}</dd>
             </div>
         )
     })
   return (
-    <div class="bg-white py-16 sm:py-16">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-            <div class="service-title">
-                <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl service_title">Our Services</p>
+    <div className="bg-light-pink py-16 sm:py-16">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="service-title">
+                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl service_title">Our Services</p>
             </div>
-            <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-                <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+                <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
                     {serviceElements}
                 </dl>
             </div>
