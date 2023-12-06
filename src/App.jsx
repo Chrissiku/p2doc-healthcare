85d6783afe81379903e662348c5223ad7ab73b8f
Chris Siku <chrissiku5@gmail.com>
2023-12-06 11:17:35 +0200

    Define Protocol

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useEffect } from "react";
 import Services from "./services/Services";
 import { useContext } from "react";
 import { Web5Context } from "./utils/Web5Context";
@@ -10,8 +10,64 @@ import Header from "./components/Header";
 const App = () => {
   const { web5, did } = useContext(Web5Context);
 
-  console.log("web5 : ", web5);
+  // console.log("web5 : ", web5);
   console.log("Did : ", did);
+
+  useEffect(() => {
+    if (web5 && did) {
+      // Configure protocol
+    }
+  }, [web5, did]);
+
+  const protocolDefinition = {
+    protocol: "https://localhost:5173",
+    published: true,
+    types: {
+      patient: {
+        schema: "https://localhost:5173/patient",
+        dataFormats: ["application/json"],
+      },
+      doctor: {
+        schema: "https://localhost:5173/doctor",
+        dataFormats: ["application/json"],
+      },
+    },
+    structure: {
+      patient: {
+        $actions: [
+          { who: "anyone", can: "write" },
+          { who: "recipient", of: "patient", can: "read" },
+        ],
+      },
+      doctor: {
+        $actions: [
+          { who: "anyone", can: "write" },
+          { who: "recipient", can: "read" },
+        ],
+      },
+    },
+  };
+
+  const installPrivateProtocol = async (web5, protocolDefinition) => {
+    return await web5.dwn.protocols.configure({
+      message: {
+        filter: {
+          protocol: protocolDefinition,
+        },
+      },
+    });
+  };
+
+  const installPublicProtocol = async (web5, did, protocolDefinition) => {
+    return await web5.dwn.protocols.configure({
+      message: {
+        filter: {
+          protocol: protocolDefinition,
+        },
+      },
+    });
+  };
+
   return (
     <>
       <Header />
