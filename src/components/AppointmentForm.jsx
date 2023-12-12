6939ba9e16c6c6dc70cf35cedee265951d49d1be
Chris Siku <chrissiku5@gmail.com>
2023-12-12 17:44:17 +0200

    Disable past data form booking

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -1,43 +1,53 @@
-import { useState, useContext } from 'react';
-import { Web5Context } from '../utils/Web5Context';
+import { useState, useContext } from "react";
+import { Web5Context } from "../utils/Web5Context";
 
 const BookingForm = () => {
-    const { web5, did, protocolDefinition } = useContext(Web5Context);
-  const [patientDID, setPatientDID] = useState('');
-  const [symptoms, setSymptoms] = useState('');
-  const [appointmentDate, setAppointmentDate] = useState('');
+  const { web5, did, protocolDefinition } = useContext(Web5Context);
+  const [patientDID, setPatientDID] = useState("");
+  const [symptoms, setSymptoms] = useState("");
+  const [appointmentDate, setAppointmentDate] = useState("");
 
-const handleSubmit = async (e) => {    
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-         console.log("Data to be sent:", {
-      patientDID,
-      symptoms,
-      appointmentDate,
-    });
-    // Create a new booking record
-    const { record, status } = await web5.dwn.records.write({
-       data: {
+      console.log("Data to be sent:", {
+        patientDID,
+        symptoms,
+        appointmentDate,
+      });
+      // Create a new booking record
+      const { record, status } = await web5.dwn.records.write({
+        data: {
           patientDID,
           symptoms,
           appointmentDate,
         },
-      message: {
-        protocol: protocolDefinition.protocol,
-        protocolPath: 'bookAppointment',
-        schema: protocolDefinition.types.bookAppointment.schema,
-        recipient: did,
-        published: true,
-      },
-    });
+        message: {
+          protocol: protocolDefinition.protocol,
+          protocolPath: "bookAppointment",
+          schema: protocolDefinition.types.bookAppointment.schema,
+          recipient: did,
+          published: true,
+        },
+      });
 
-        await record.send(did);
-        console.log("success")
+      await record.send(did);
+      console.log("success");
+    } catch (error) {
+      console.error("Error booking appointment:", error);
+    }
+  };
 
-  } catch (error) {
-    console.error('Error booking appointment:', error);
-  }
-};
+  const getCurrentDateTime = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const day = String(now.getDate()).padStart(2, "0");
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -64,6 +74,7 @@ const handleSubmit = async (e) => {
           type="datetime-local"
           value={appointmentDate}
           onChange={(e) => setAppointmentDate(e.target.value)}
+          min={getCurrentDateTime()}
           required
         />
       </div>
